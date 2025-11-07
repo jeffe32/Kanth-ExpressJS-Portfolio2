9cb4af493b54b450097ed0efe2fcb7179697d739
jeffe32 <rishikanth.manotheepan@ontariotechu.net>
2025-11-07 10:24:56 -0800

    first commit

---
+++ Kanth-ExpressJS-Portfolio2/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Contacts page. */
+router.get('/Contacts', function(req, res, next) {
+  res.render('index', { title: 'Contacts' });
+});
+
+module.exports = router;
